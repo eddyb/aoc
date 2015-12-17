ec2e15b8c8dccfae1906f0f03e27ac7ee2aec126
Eduard Burtescu <edy.burt@gmail.com>
2015-12-17 19:09:05 +0200

    Day 1, part 2.

--- 1.rs
+++ 1.rs
@@ -1,7 +1,11 @@
 fn main() {
     let mut depth = 0;
-    for &c in &include_bytes!("1.in")[..] {
+    for (i, &c) in include_bytes!("1.in").iter().enumerate() {
         depth += ((c as i32) << 31 >> 31) | 1;
+        if cfg!(part2) && depth == -1 {
+            println!("{}", i + 1);
+            return;
+        }
     }
     println!("{}", depth);
 }
